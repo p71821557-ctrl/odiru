677105f48f4ba51907e8e2a57dcde2335827c8d7
p7182 <p71821557@gmail.com>
2026-05-16 21:37:26 +0900

    first upload

--- public/pages/login/js/script.js
+++ public/pages/login/js/script.js
@@ -1,22 +1,16 @@
-console.log("로그인 페이지 연결 완료");
+console.log("ODIRU LOGIN");
 
-const googleButtons = document.querySelectorAll(".google-btn");
-const kakaoButtons = document.querySelectorAll(".kakao-btn");
+// 카카오 버튼
+const kakaoButtons =
+  document.querySelectorAll(".kakao-btn");
 
-// Google 로그인
-googleButtons.forEach((button) => {
-
-  button.addEventListener("click", () => {
-    alert("Google 로그인 준비중");
-  });
-
-});
-
-// Kakao 로그인
 kakaoButtons.forEach((button) => {
 
-  button.addEventListener("click", () => {
-    alert("Kakao 로그인 준비중");
+  button.addEventListener("mouseenter", () => {
+
+    button.style.transform =
+      "translateY(-3px)";
+
   });
 
 });
