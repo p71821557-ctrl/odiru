649ba6c3b3da2fb5bdbfc4b822c101e2f679025f
p7182 <p71821557@gmail.com>
2026-05-23 21:10:03 +0900

    첫 커밋

--- sever.js
+++ sever.js
@@ -52,6 +52,7 @@ passport.use(
   new KakaoStrategy(
     {
       clientID: "05d30bdbb381878c14e4d47a15d86d8f",
+      clientSecret: "nGfYod9dIitR4VjgYfX3kSNGkTaZjqzK",
       callbackURL: "http://localhost:3000/auth/kakao/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
