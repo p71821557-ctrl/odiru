08034f7e19fdd01a60014aa0e9be43d27a803f36
p7182 <p71821557@gmail.com>
2026-05-26 11:16:21 +0900

    첫 커밋

--- public/pages/login/js/script.js
+++ public/pages/login/js/script.js
@@ -1,14 +1,16 @@
 console.log("ODIRU LOGIN");
 
-// 카카오 버튼
 
+
+// ==========================================
+// 카카오 버튼
+// ==========================================
 const kakaoButtons =
 document.querySelectorAll(".kakao-btn");
 
 kakaoButtons.forEach((button) => {
 
   // hover
-
   button.addEventListener(
     "mouseenter",
     () => {
@@ -32,7 +34,6 @@ kakaoButtons.forEach((button) => {
 
 
   // 클릭 시 카카오 로그인 이동
-
   button.addEventListener(
     "click",
     () => {
@@ -44,3 +45,95 @@ kakaoButtons.forEach((button) => {
   );
 
 });
+
+
+
+// ==========================================
+// 로그인 상태 체크
+// ==========================================
+async function checkLogin() {
+
+  try {
+
+    const res =
+      await fetch("/api/user");
+
+    const user =
+      await res.json();
+
+    // 로그인 안 됨
+    if (!user) {
+
+      console.log("비로그인 상태");
+
+      return;
+
+    }
+
+    console.log("로그인 유저:", user);
+
+    // 버튼 텍스트 변경
+    kakaoButtons.forEach((button) => {
+
+      button.innerHTML = `
+        👋 ${user.nickname}님
+      `;
+
+      // 클릭 시 마이페이지 이동
+      button.onclick = () => {
+
+        window.location.href =
+        "/pages/mypage/mypage.html";
+
+      };
+
+    });
+
+
+
+
+    // ==========================================
+    // 상단 프로필 자동 추가
+    // ==========================================
+    const profileArea =
+      document.getElementById(
+        "profile-area"
+      );
+
+    if (profileArea) {
+
+      profileArea.innerHTML = `
+
+        <div class="login-user">
+
+          <img
+            src="${user.profileImage}"
+            class="top-profile"
+          />
+
+          <span>
+            ${user.nickname}님
+          </span>
+
+        </div>
+
+      `;
+
+    }
+
+  }
+
+  catch (err) {
+
+    console.log(err);
+
+  }
+
+}
+
+
+
+// ==========================================
+// 실행
+// ==========================================
+checkLogin();
